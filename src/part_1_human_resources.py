88f5e604f60c1e04bbb2728543309d2ae96f3f26
Joe Mulvey <joe.mulvey@northcoders.com>
2024-01-02 14:08:52 +0000

    [DEC-193] skeleton of first section

---
+++ src/part_1_human_resources.py
@@ -0,0 +1,8 @@
+def make_name_tags():
+    # implement me!
+    pass
+
+
+def create_poll():
+    # implement me
+    pass
---
+++ test/test_part_1_human_resources.py
@@ -0,0 +1,11 @@
+import pytest
+from src.part_1_human_resources import make_name_tags, create_poll
+
+def test_make_name_tags_does_something():
+    # implement test
+    pass
+
+
+def test_create_poll_does_something():
+    # implement test
+    pass
