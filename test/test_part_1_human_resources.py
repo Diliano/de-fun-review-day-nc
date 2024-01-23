c825a6d056caeacc769c86e4aa1fe4096ae61e64
Joe Mulvey <joe.mulvey@northcoders.com>
2024-01-23 16:08:03 +0000

    [DEC-193] remove test hints

--- test/test_part_1_human_resources.py
+++ test/test_part_1_human_resources.py
@@ -1,10 +1,3 @@
 from src.part_1_human_resources import make_name_tags, create_poll
 
-def test_make_name_tags_does_something():
-    # implement test
-    pass
-
-
-def test_create_poll_does_something():
-    # implement test
-    pass
+# implement your tests here
