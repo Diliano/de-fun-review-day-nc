d28fa181fa7ff8389abff1ad6020233435d45004
DCrawley94 <danikacrawley.dev@gmail.com>
2024-08-12 15:37:49 +0100

    remove testdox from vending machine

--- test/test_part_2_vending_machine.py
+++ test/test_part_2_vending_machine.py
@@ -2,45 +2,37 @@ import pytest
 from src.part_2_vending_machine import VendingMachine
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def machine():
     return VendingMachine()
 
 
-@pytest.mark.describe('All Vending Machine Methods')
 class TestVendingMachine:
-
-    @pytest.mark.it('has valid credit attribute')
-    def test_credit(self, machine):
+    def test_initial_credit_set_to_0(self, machine):
         assert machine.credit == 0
 
-    @pytest.mark.it('has expected stock attributes')
-    def test_stock(self, machine):
-        assert machine.stock['A'] == {}
-        assert machine.stock['B'] == {}
-        assert machine.stock['C'] == {}
+    def test_initial_stock(self, machine):
+        assert machine.stock["A"] == {}
+        assert machine.stock["B"] == {}
+        assert machine.stock["C"] == {}
 
-    @pytest.mark.it('adds credit to initial amount')
-    def test_add_credit(self, machine):
+    def test_add_credit_method_sets_credit_attribute(self, machine):
         machine.add_credit(75)
         assert machine.credit == 75
         new_machine = VendingMachine()
         new_machine.add_credit(62)
         assert new_machine.credit == 62
 
-    @pytest.mark.it('accumulates credit')
-    def test_accumulate_credit(self, machine):
+    def test_add_credit_increases_current_credit(self, machine):
         machine.add_credit(19)
         assert machine.credit == 19
         machine.add_credit(33)
         assert machine.credit == 52
 
-    @pytest.mark.it('validates when there is sufficient credit for an item')
-    def test_sufficient_credit(self, machine):
+    def test_credit_checker_returns_true_for_sufficient_credit(self, machine):
         machine.add_credit(36)
-        assert machine.credit_checker(30)
+        assert machine.credit_checker(30) is True
 
-    @pytest.mark.it('validates when there is insufficient credit for an item')
-    def test_insufficient_credit(self, machine):
+    def test_credit_checker_returns_false_for_insufficient_credit(self, machine):
         machine.add_credit(36)
-        assert not machine.credit_checker(44)
+        assert machine.credit_checker(44) is False
