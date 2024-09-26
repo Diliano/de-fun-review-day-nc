87ec7df8ccb0321847505adc68c3d36afbf61f0b
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-26 16:43:33 +0100

    add functionality and passing tests for the flatten() function of part_5_recursion.py

--- src/part_5_recursion.py
+++ src/part_5_recursion.py
@@ -1,7 +1,13 @@
-def flatten():
-    # implement me
-    pass
+def flatten(lst, depth=1):
+    result = []
 
+    for element in lst:
+        if isinstance(element, list) and depth > 0:
+            result.extend(flatten(element, depth - 1))
+        else:
+            result.append(element)
+
+    return result
 
 def deep_entries():
     # implement me
--- test/test_part_5_recursion.py
+++ test/test_part_5_recursion.py
@@ -1,1 +1,49 @@
-# Your tests here
+from src.part_5_recursion import flatten
+
+
+class TestFlatten:
+    def test_returns_empty_list_when_given_empty_list(self):
+        assert flatten([]) == []
+
+    def test_returns_given_list_if_unnested(self):
+        assert flatten([1, 2, 3]) == [1, 2, 3]
+
+    def test_flattens_list_containing_one_nested_list_with_default_depth_1(self):
+        assert flatten([1, 2, [3, 4]]) == [1, 2, 3, 4]
+
+    def test_flattens_list_containing_multiple_nested_lists_with_default_depth_1(self):
+        # Arrange
+        test_input = [1, 2, [3, [4, [5, 6]]]]
+        expected = [1, 2, 3, [4, [5, 6]]]
+        # Act
+        result = flatten(test_input)
+        # Assert
+        assert result == expected
+
+    def test_flattens_list_containing_multiple_nested_lists_with_given_depth(self):
+        # Arrange
+        test_input = [1, 2, [3, [4, [5, 6]]]]
+        test_depth = 2
+        expected = [1, 2, 3, 4, [5, 6]]
+        # Act
+        result = flatten(test_input, test_depth)
+        # Assert
+        assert result == expected
+
+        # Arrange
+        test_input = [1, 2, [3, [4, [5, 6]]]]
+        test_depth = 3
+        expected = [1, 2, 3, 4, 5, 6]
+        # Act
+        result = flatten(test_input, test_depth)
+        # Assert
+        assert result == expected
+
+        # Arrange
+        test_input = [1, 2, [3, [4, [5, 6]]]]
+        test_depth = 25
+        expected = [1, 2, 3, 4, 5, 6]
+        # Act
+        result = flatten(test_input, test_depth)
+        # Assert
+        assert result == expected
