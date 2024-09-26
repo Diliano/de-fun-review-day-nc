9b87df805f130db03d3d34cb4872ef0e6a78a3cd
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-26 13:52:11 +0100

    add functionality and passing tests for the indexer() function, as part of part_4_iterators.py

--- src/part_4_iterators.py
+++ src/part_4_iterators.py
@@ -1,11 +1,8 @@
 def indexer(text):
     if text:
         for index, c in enumerate(text):
-            if index == 0 and not c.isspace():
+            if (index == 0 and not c.isspace()) or (not c.isspace() and text[index - 1].isspace()):
                 yield index
-            else:
-                if not c.isspace() and text[index - 1].isspace():
-                    yield index
 
 def cool_cat():
     # implement me
