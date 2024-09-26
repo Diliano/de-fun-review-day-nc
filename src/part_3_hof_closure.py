a5bfb6adafaa2ac8be5482d0baf69b5ae90a448f
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-26 11:34:17 +0100

    add functionality and passing tests for the generate_multiples() function, as part of part_3_hof_closure.py

--- src/part_3_hof_closure.py
+++ src/part_3_hof_closure.py
@@ -1,6 +1,7 @@
-def generate_multiples():
-    # implement me
-    pass
+def generate_multiples(multiple):
+    def generate_list_of_multiples(list_length):
+        return [multiple * i for i in range(1, list_length + 1)]
+    return generate_list_of_multiples
 
 
 def capitaliser():
--- test/test_part_3_hof_closure.py
+++ test/test_part_3_hof_closure.py
@@ -4,21 +4,17 @@ from types import FunctionType
 
 
 class TestGenerateMultiples:
-    @pytest.mark.skip
     def test_returns_function(self):
         assert type(generate_multiples(4)) is FunctionType
 
-    @pytest.mark.skip
     def test_new_func_returns_empty_list_when_passed_0(self):
         mults_5 = generate_multiples(5)
         assert mults_5(0) == []
 
-    @pytest.mark.skip
-    def test_new_func_returns_single_item_list_when_passed_0(self):
+    def test_new_func_returns_single_item_list_when_passed_1(self):
         mults_5 = generate_multiples(5)
         assert mults_5(1) == [5]
 
-    @pytest.mark.skip
     def test_new_func_returns_correct_number_of_items_in_list(self):
         mults_5 = generate_multiples(5)
         assert mults_5(3) == [5, 10, 15]
@@ -28,125 +24,125 @@ class TestGenerateMultiples:
         assert mults_7(6) == [7, 14, 21, 28, 35, 42]
 
 
-class TestCapitaliser:
-    @pytest.mark.skip
-    def test_returns_function(self):
-        def example_func():
-            pass
+# class TestCapitaliser:
+#     @pytest.mark.skip
+#     def test_returns_function(self):
+#         def example_func():
+#             pass
 
-        result = capitaliser(example_func)
+#         result = capitaliser(example_func)
 
-        assert isinstance(result, FunctionType)
+#         assert isinstance(result, FunctionType)
 
-    @pytest.mark.skip
-    def test_enclosed_func_invoked_only_once(self):
-        count = 0
-        def example_func():
-            nonlocal count
-            count += 1
-            return 'Count is 1'
+#     @pytest.mark.skip
+#     def test_enclosed_func_invoked_only_once(self):
+#         count = 0
+#         def example_func():
+#             nonlocal count
+#             count += 1
+#             return 'Count is 1'
         
-        decorated_func = capitaliser(example_func)
-        decorated_func()
+#         decorated_func = capitaliser(example_func)
+#         decorated_func()
         
-        assert count == 1
+#         assert count == 1
 
-    @pytest.mark.skip
-    def test_decorated_func_returns_capitalised_string_result(self):
-        def example_func():
-            return "hello world"
+#     @pytest.mark.skip
+#     def test_decorated_func_returns_capitalised_string_result(self):
+#         def example_func():
+#             return "hello world"
 
-        decorated_func = capitaliser(example_func)
-        result = decorated_func()
+#         decorated_func = capitaliser(example_func)
+#         result = decorated_func()
 
-        assert result == "HELLO WORLD"
+#         assert result == "HELLO WORLD"
 
-    @pytest.mark.skip
-    def test_decorated_func_returns_int_result_unchanged(self):
-        def example_func():
-            return 14
+#     @pytest.mark.skip
+#     def test_decorated_func_returns_int_result_unchanged(self):
+#         def example_func():
+#             return 14
 
-        decorated_func = capitaliser(example_func)
-        result = decorated_func()
+#         decorated_func = capitaliser(example_func)
+#         result = decorated_func()
 
-        assert result == 14
+#         assert result == 14
 
-    @pytest.mark.skip
-    def test_decorated_func_returns_bool_result_unchanged(self):
-        def example_func():
-            return False
+#     @pytest.mark.skip
+#     def test_decorated_func_returns_bool_result_unchanged(self):
+#         def example_func():
+#             return False
 
-        decorated_func = capitaliser(example_func)
-        result = decorated_func()
+#         decorated_func = capitaliser(example_func)
+#         result = decorated_func()
 
-        assert result is False
+#         assert result is False
 
-    @pytest.mark.skip
-    def test_function_acts_as_decorator(self):
-        @capitaliser
-        def example_func():
-            return "hey hey it's decorating time"
+#     @pytest.mark.skip
+#     def test_function_acts_as_decorator(self):
+#         @capitaliser
+#         def example_func():
+#             return "hey hey it's decorating time"
 
-        result = example_func()
-        assert result == "HEY HEY IT'S DECORATING TIME"
+#         result = example_func()
+#         assert result == "HEY HEY IT'S DECORATING TIME"
 
 
-class TestSecureFunction:
-    @pytest.mark.skip
-    def test_func_with_no_args_invoked_with_correct_password(self):
-        @secure_func("password")
-        def func_no_args():
-            return "Function ran successfully!"
+# class TestSecureFunction:
+#     @pytest.mark.skip
+#     def test_func_with_no_args_invoked_with_correct_password(self):
+#         @secure_func("password")
+#         def func_no_args():
+#             return "Function ran successfully!"
 
-        result = func_no_args("password")
-        assert result == "Function ran successfully!"
+#         result = func_no_args("password")
+#         assert result == "Function ran successfully!"
 
-    @pytest.mark.skip
-    def test_func_with_no_args_errors_on_bad_password(self):
-        @secure_func("password")
-        def func_no_args():
-            return "Function ran successfully!"
+#     @pytest.mark.skip
+#     def test_func_with_no_args_errors_on_bad_password(self):
+#         @secure_func("password")
+#         def func_no_args():
+#             return "Function ran successfully!"
 
-        result = func_no_args("eggy_bread")
+#         result = func_no_args("eggy_bread")
 
-        assert result == "Sorry, your password is incorrect!"
+#         assert result == "Sorry, your password is incorrect!"
 
-    @pytest.mark.skip
-    def test_func_with_args_invoked_with_correct_password(self):
-        @secure_func("P4s5w0rd!!!")
-        def func_with_args(*args):
-            return list(args)
+#     @pytest.mark.skip
+#     def test_func_with_args_invoked_with_correct_password(self):
+#         @secure_func("P4s5w0rd!!!")
+#         def func_with_args(*args):
+#             return list(args)
 
-        result = func_with_args("P4s5w0rd!!!", 1, 2, 3)
+#         result = func_with_args("P4s5w0rd!!!", 1, 2, 3)
 
-        assert result == [1, 2, 3]
+#         assert result == [1, 2, 3]
 
-    @pytest.mark.skip
-    def test_func_with_args_errors_on_bad_password(self):
-        @secure_func("P4s5w0rd!!!")
-        def func_with_args(*args):
-            return list(args)
+#     @pytest.mark.skip
+#     def test_func_with_args_errors_on_bad_password(self):
+#         @secure_func("P4s5w0rd!!!")
+#         def func_with_args(*args):
+#             return list(args)
 
-        result = func_with_args("Th15_1s_WR0ng", 1, 2, 3)
+#         result = func_with_args("Th15_1s_WR0ng", 1, 2, 3)
 
-        assert result == "Sorry, your password is incorrect!"
+#         assert result == "Sorry, your password is incorrect!"
 
-    @pytest.mark.skip
-    def test_args_and_kwargs_invoked_with_correct_password(self):
-        @secure_func("P4s5w0rd!")
-        def func_with_args_and_kwargs(*args, **kwargs):
-            return (list(args), kwargs)
+#     @pytest.mark.skip
+#     def test_args_and_kwargs_invoked_with_correct_password(self):
+#         @secure_func("P4s5w0rd!")
+#         def func_with_args_and_kwargs(*args, **kwargs):
+#             return (list(args), kwargs)
 
-        result = func_with_args_and_kwargs("P4s5w0rd!", 1, 2, 3, invoked=True)
+#         result = func_with_args_and_kwargs("P4s5w0rd!", 1, 2, 3, invoked=True)
 
-        assert result == ([1, 2, 3], {"invoked": True})
+#         assert result == ([1, 2, 3], {"invoked": True})
 
-    @pytest.mark.skip
-    def test_func_with_args_and_kwargs_errors_on_bad_password(self):
-        @secure_func("P4s5w0rd!!!")
-        def func_with_args_and_kwargs(*args, **kwargs):
-            return (list(args), dict(kwargs))
+#     @pytest.mark.skip
+#     def test_func_with_args_and_kwargs_errors_on_bad_password(self):
+#         @secure_func("P4s5w0rd!!!")
+#         def func_with_args_and_kwargs(*args, **kwargs):
+#             return (list(args), dict(kwargs))
 
-        result = func_with_args_and_kwargs("3ggy_bRe4d", 1, 2, invoked=False)
+#         result = func_with_args_and_kwargs("3ggy_bRe4d", 1, 2, invoked=False)
 
-        assert result == "Sorry, your password is incorrect!"
+#         assert result == "Sorry, your password is incorrect!"
