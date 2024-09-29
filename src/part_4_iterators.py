9f4f8a3f9515125d7b8dd9e4dc6ad0b148759131
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-29 14:41:43 +0100

    refactored part_4_iterators.py indexer() function to use char syntax instead of c, in line with using index rather than i

--- src/part_4_iterators.py
+++ src/part_4_iterators.py
@@ -1,7 +1,7 @@
 def indexer(text):
     if text:
-        for index, c in enumerate(text):
-            if (index == 0 and not c.isspace()) or (not c.isspace() and text[index - 1].isspace()):
+        for index, char in enumerate(text):
+            if (index == 0 and not char.isspace()) or (not char.isspace() and text[index - 1].isspace()):
                 yield index
 
 def cool_cat(*args):
